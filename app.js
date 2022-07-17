ad7efffb3e6066be08aa0fdeb09d20cefd4b30cd
RuiPSDev <rfilho60@me.com>
2022-07-17 09:18:24 -0400

    Added code to make oAuth2.0 login through Facebook

--- app.js
+++ app.js
@@ -1,57 +1,109 @@
-const express         =     require('express')
-  , passport          =     require('passport')
-  , cookieParser      =     require('cookie-parser')
-  , session           =     require('express-session')
-  , bodyParser        =     require('body-parser')
-  , config            =     require('./configuration/config')
-  , app               =     express();
+const express = require("express"),
+  passport = require("passport"),
+  cookieParser = require("cookie-parser"),
+  session = require("express-session"),
+  bodyParser = require("body-parser"),
+  config = require("./configuration/config"),
+  configFacebook = require("./configuration/configFacebook"),
+  app = express();
 
-var GoogleStrategy = require('passport-google-oauth20').Strategy;
+var GoogleStrategy = require("passport-google-oauth20").Strategy;
+var FacebookStrategy = require("passport-facebook").Strategy;
+const PORT = 3000;
 
-passport.use(new GoogleStrategy({
-    clientID: config.api_key,
-    clientSecret: config.api_secret,
-    callbackURL: config.callback_url
-  },
-  function(accessToken, refreshToken, profile, done) {
-    return done(null, profile);
-  }
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: config.api_key,
+      clientSecret: config.api_secret,
+      callbackURL: config.callback_url,
+    },
+    function (accessToken, refreshToken, profile, done) {
+      return done(null, profile);
+    }
+  )
+);
+
+passport.use(
+  new FacebookStrategy(
+    {
+      clientID: configFacebook.api_key,
+      clientSecret: configFacebook.api_secret,
+      callbackURL: configFacebook.callback_url,
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      User.findOrCreate({ facebookId: profile.id }, function (err, user) {
+        return cb(err, user);
+      });
+    }
 ));
 
 // Passport session setup.
-passport.serializeUser(function(user, done) {
+passport.serializeUser(function (user, done) {
   done(null, user);
 });
 
-passport.deserializeUser(function(obj, done) {
+passport.deserializeUser(function (obj, done) {
   done(null, obj);
 });
 
-app.set('views', __dirname + '/views');
-app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(session({ secret: 'keyboard cat', resave: true, saveUninitialized: true }));
+app.use(
+  session({ secret: "keyboard cat", resave: true, saveUninitialized: true })
+);
 app.use(passport.initialize());
 app.use(passport.session());
-app.use(express.static(__dirname + '/public'));
+app.use(express.static(__dirname + "/public"));
 
-app.get('/', function(req, res){
-  res.render('index', { user: req.user });
+// ********** Entra no root e chama o template EJS **********
+app.get("/", function (req, res) {
+  res.render("index", { user: req.user });
 });
 
-app.get('/auth/google', passport.authenticate('google', { scope: ['profile'] }));
+// **********  oAuth2.0 com Google **********
 
-app.get('/auth/google/callback', 
-  passport.authenticate('google',  { successRedirect : '/', failureRedirect: '/login' }),
-  function(req, res) {
-    res.redirect('/');
+app.get(
+  "/auth/google",
+  passport.authenticate("google", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/google/callback",
+  passport.authenticate("google", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
+  function (req, res) {
+    res.redirect("/");
   }
 );
 
-app.get('/logout', function(req, res){
+// **********  oAuth2.0 com Facebook **********
+
+app.get(
+  "/auth/facebook",
+  passport.authenticate("facebook", { scope: ["profile"] })
+);
+
+app.get(
+  "/auth/facebook/callback",
+  passport.authenticate("facebook", {
+    successRedirect: "/",
+    failureRedirect: "/login",
+  }),
+  function (req, res) {
+    res.redirect("/");
+  }
+);
+
+// **********  logout  **********
+
+app.get("/logout", function (req, res) {
   req.logout();
-  res.redirect('/');
+  res.redirect("/");
 });
 
-app.listen(3000, () => console.log('Server up'));
+app.listen(PORT, () => console.log(`Server up at: http://localhost:${PORT}`));
--- configuration/config.js
+++ configuration/config.js
@@ -1,5 +1,5 @@
 module.exports={
-    "api_key": "yourKey.apps.googleusercontent.com",
-    "api_secret": "secretKey",
+    "api_key": "1010796475153-fe6baegg54r7na22q1fm7rfhkn430q7b.apps.googleusercontent.com",
+    "api_secret": "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",
     "callback_url": "http://localhost:3000/auth/google/callback"
 }
